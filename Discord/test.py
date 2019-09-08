94ac9892b36bc3cb5e57de6536134fe4f88ab5ad
PC R <prohenho7138@gmail.com>
2019-09-08 10:44:42 +0000

    yeah

--- Discord/test.py
+++ Discord/test.py
@@ -1,23 +1,25 @@
-import discord    #디스코드 들고옴
+import discord
+import asyncio
 
 client = discord.Client()
+token = "NjIwMTM3NTY0ODQxNTc0NDIx.XXSa_g.86uJbhZaahja2XuduHHFNW-HM_E"
 
 @client.event
-async def on_ready() :
-    #처음 접속할시
-    print("아아 봇준비완료~")
+async def on_ready():
+    print("현재 계정으로 로그인 합니다 : ")
     print(client.user.name)
     print(client.user.id)
-    await client.change_presence (game=discord.Game(name="봇 만들어 보는중"))
+    print("===================")
+    await client.change_presence (game=discord.Game(name="봇 만들어 보는중", type=1))
     
+
 @client.event
-async def on_message() :
-    #만약 유저로부터 다른 말이 나온다면
-    if message.author == cliet.user :
+async def on_message():
+    if message.author == client.user:
         return
     if message.content == "!" :
         await client.send_message (message.channel, "아아 테스트으으")
     if message.content == "!아아" :
-        await client.send_message (message.channel, "예에에")
+        await client.send_message (message.channel, "예에에")        
         
-client.run('NjIwMTM3NTY0ODQxNTc0NDIx.XXSa_g.86uJbhZaahja2XuduHHFNW-HM_E')
+client.run(token)
