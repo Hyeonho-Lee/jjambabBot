005514a7997db0a475e14796100d0fcc908688d5
leehyeonho <54760301+Hyeonho-Lee@users.noreply.github.com>
2019-09-12 17:57:37 +0900

    Update test.py

--- test.py
+++ test.py
@@ -61,5 +61,6 @@ async def on_message(message):
         embed.set_footer(text=date_message.todayT)
         await client.send_message(message.channel, embed=embed)
       
-token = os.environ['BOT_TOKEN']
+#token = os.environ['BOT_TOKEN']
+token = 'NjIwMTM3NTY0ODQxNTc0NDIx.XXoD7g.YWOzNlqje5Otb7cWzyCFDGqPw0w'
 client.run(token)
