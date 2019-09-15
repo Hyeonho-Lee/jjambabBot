a0b90a2b435660a3084a9e71e4c8b9b7889100d8
PC R <prohenho7138@gmail.com>
2019-09-15 09:31:38 +0000

    yea

--- jjambab_message.py
+++ jjambab_message.py
@@ -45,17 +45,17 @@ def search_jjambab(result):
     search_breakfast = sheet.cell(search + 1,2).value
     search_lunch = sheet.cell(search + 1, 3).value
     search_dinner = sheet.cell(search + 1, 4).value
-    search_result = str(date_message.todayM) + "월" + str(search) + "일" + " 짬밥입니다\n"
+    search_result = "단결! " + str(date_message.todayM) + "월" + str(search) + "일" + " 짬밥입니다!!\n"
     searchs_result = "==========아침==========\n" + search_breakfast + "\n==========점심==========\n" + search_lunch + "\n==========저녁==========\n" + search_dinner + "\n========================"
     return search_result, searchs_result
 
-today_result = str(date_message.todayM) + "월" + str(date_message.todayD) + "일" + " 짬밥입니다\n"
+today_result = "단결! " + str(date_message.todayM) + "월" + str(date_message.todayD) + "일" + " 짬밥입니다!!\n"
 todays_result = "==========아침==========\n" + today_breakfast + "\n==========점심==========\n" + today_lunch + "\n==========저녁==========\n" + today_dinner + "\n========================"
 
-tomorrow_result = str(date_message.todayM) + "월" + str(date_message.tomorrowD) + "일" + " 짬밥입니다\n"
+tomorrow_result = "단결! " + str(date_message.todayM) + "월" + str(date_message.tomorrowD) + "일" + " 짬밥입니다!!\n"
 tomorrows_result = "==========아침==========\n" + tomorrow_breakfast + "\n==========점심==========\n" + tomorrow_lunch + "\n==========저녁==========\n" + tomorrow_dinner + "\n========================"
 
-yesterday_result = str(date_message.todayM) + "월" + str(date_message.yesterdayD) + "일" + " 짬밥입니다\n"
+yesterday_result = "단결! " + str(date_message.todayM) + "월" + str(date_message.yesterdayD) + "일" + " 짬밥입니다!!\n"
 yesterdays_result = "==========아침==========\n" + yesterday_breakfast + "\n==========점심==========\n" + yesterday_lunch + "\n==========저녁==========\n" + yesterday_dinner + "\n========================"
 
 #search_jjambab(10)
