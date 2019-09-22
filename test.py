48c333cdbad6d184c4d791088dbfa7c7de28a740
leehyeonho <54760301+Hyeonho-Lee@users.noreply.github.com>
2019-09-22 15:47:27 +0900

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import re
 #---------------------#
 import date_message
 import jjambab_message
-import on_mess
+import work_message
 #---------------------#
 
 client = discord.Client()
@@ -24,7 +24,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print("===================")
-    game = discord.Game("RPC봇 테스트2")
+    game = discord.Game("RPC봇 테스트")
     await client.change_presence(status=discord.Status.online, activity=game)
 
 #########################################################################
@@ -36,27 +36,73 @@ async def on_message(message):
         return
     
     if message.content.startswith("/?"):
-        await message.channel.send("단결!! \n명령어! /오늘 짬밥 \n/내일 짬밥 \n/어제 짬밥 \n/(원하는 일자)일 짬밥")
-    
-    if message.content.startswith("/오늘 짬밥"):
-        await message.channel.send(embed=set_embed(jjambab_message.today_result, jjambab_message.todays_result))
-        
-    if message.content.startswith("/내일 짬밥"):        
-        await message.channel.send(embed=set_embed(jjambab_message.tomorrow_result, jjambab_message.tomorrows_result))
-        
-    if message.content.startswith("/어제 짬밥"):
-        await message.channel.send(embed=set_embed(jjambab_message.yesterday_result, jjambab_message.yesterdays_result))
+        await message.channel.send(embed=set_embed("단결! 명령어를 불러드렸습니다!!", "/(오늘, 내일, 어제) 짬밥\n/(원하는 일자)일 짬밥\n/px"))
         
     search_day = ""
-
+    
     last_text = message.content
     result_day = re.findall("\d+", last_text)
     for result in result_day:
         search_day = result
     if message.content == "/%s일 짬밥"%search_day:
-        test1, test2 = jjambab_message.search_jjambab(search_day)
-        await message.channel.send(embed=set_embed(test1, test2))
+        title, description = jjambab_message.search_jjambab(search_day)
+        await message.channel.send(embed=set_embed(title, description))
+    
+    if message.content.startswith("/오늘 짬밥"):
+        title, description = jjambab_message.result_jjambab("오늘")
+        await message.channel.send(embed=set_embed(title, description))
         
+    if message.content.startswith("/내일 짬밥"):
+        title, description = jjambab_message.result_jjambab("내일")
+        await message.channel.send(embed=set_embed(title, description))
+        
+    if message.content.startswith("/어제 짬밥"):
+        title, description = jjambab_message.result_jjambab("어제")
+        await message.channel.send(embed=set_embed(title, description))
+        
+    if message.content.startswith("/px"):
+        title = "단결! px이용시간 입니다!!"
+        description = "==========평일==========\n10:30 ~ 11:50\n13:00 ~ 17:00 \n 18:00 ~ 19:30\n==========휴일==========\n14:00 ~ 17:30\n========================"
+        await message.channel.send(embed=set_embed(title, description))
+    
+    index = ""
+    date = ""
+    writer = ""
+    time = ""
+    content = ""
+    
+    if message.content == "/근무":
+        title, description = work_message.result_work("근무")
+        await message.channel.send(embed=set_embed(title, description))
+    
+    last_text = message.content
+    result_text = re.findall("\w+", last_text)
+    #print(result_text)
+    result_len = len(result_text)
+
+    if message.content.startswith("/근무 추가"):
+        if result_len == 6:
+            date = result_text[2]
+            writer = result_text[3]
+            time = result_text[4]
+            content = result_text[5]
+        else:
+            return
+        
+    if message.content == "/근무 추가 %s,%s,%s,%s"%(date,writer,time,content):
+        last_index = work_message.last_index()
+        work_message.work_write(last_index+1,date,writer,time,content,last_index+1)
+        title, description = work_message.result_work("근무 추가")
+        await message.channel.send(embed=set_embed(title, description))
+        
+    if message.content == "/근무 보기":
+        last_index = work_message.last_index()
+        index,date,writer,time,content = work_message.work_all_load()
+        title, description = work_message.result_work("근무 보기")
+        for i in range(0,last_index-1):
+            text = index[i] + " " + date[i] + " " + writer[i] + " " + time[i] + " " + content[i]
+            await message.channel.send(embed=set_embed(title, text))
+
 #########################################################################
 
-client.run("NjIwMTM3NTY0ODQxNTc0NDIx.XX3jVA.MDMGx1RSRGWHZnr-sF8mBHS-1GY")
+client.run("NjIwMTM3NTY0ODQxNTc0NDIx.XYDeDA.sHhlPOC2tycUe1nxQ-nkjsPN8KQ")
