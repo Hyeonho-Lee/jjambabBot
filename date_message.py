2c48d8acba08fed152eda61cbd38fdbf0f36d7b9
leehyeonho <54760301+Hyeonho-Lee@users.noreply.github.com>
2019-10-27 10:34:24 +0900

    Update date_message.py

--- date_message.py
+++ date_message.py
@@ -8,6 +8,7 @@ KR_sloct = KRT.localize(datetime.datetime.now())
 
 todayD = KR_sloct.strftime('%d')
 todayM = KR_sloct.strftime('%m')
+todayY = KR_sloct.strftime('%y')
 todayDate = KR_sloct.strftime('%Y-%m-%d')
 todayTime = KR_sloct.strftime('%H:%M:%S')
 todayTimeS = KR_sloct.strftime('%S')
@@ -32,6 +33,7 @@ def reload_today():
 	
     todayD = KR_sloct.strftime('%d')
     todayM = KR_sloct.strftime('%m')
+    todayY = KR_sloct.strftime('%y')
     todayDate = KR_sloct.strftime('%Y-%m-%d')
     todayTime = KR_sloct.strftime('%H:%M:%S')
     todayTimeS = KR_sloct.strftime('%S')
@@ -39,7 +41,7 @@ def reload_today():
     todayTimeH = KR_sloct.strftime('%H')
 
     todays = KR_sloct.strftime('오늘은 %m월 %d일 입니다.')
-    todayT = str(KR_sloct.year) + "년 " + str(KR_sloct.month) + "월 " + str(KR_sloct.day) + "일"
+    todayT = todayY + "년 " + todayM + "월 " + todayD + "일"
     tomorrow = KR_sloct + datetime.timedelta(days=1)
     tomorrows = tomorrow.strftime('내일은 %m월 %d일 입니다.')
     tomorrowD = tomorrow.strftime('%d')
