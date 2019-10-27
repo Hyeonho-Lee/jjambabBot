e8ee6e0089f43dfb60d87ac36f7184aac5aedc75
leehyeonho <54760301+Hyeonho-Lee@users.noreply.github.com>
2019-10-27 18:44:42 +0900

    Update jjambab_message.py

--- jjambab_message.py
+++ jjambab_message.py
@@ -17,52 +17,43 @@ sheet = clients.open("2019-10 jjambab").sheet1
     
 data = sheet.get_all_records()
 
-todayD = int(date_message.todayD)
-todayM = int(date_message.todayM)
-tomorrowD = int(date_message.tomorrowD)
-yesterdayD= int(date_message.yesterdayD)
-
 for i in range(0, 32):
 
-    if i == todayD:
-        today_breakfast = sheet.cell(todayD + 1,2).value
-        today_lunch = sheet.cell(todayD + 1, 3).value
-        today_dinner = sheet.cell(todayD + 1, 4).value
+    if i == int(date_message.todayD):
+        today_breakfast = sheet.cell(int(date_message.todayD) + 1,2).value
+        today_lunch = sheet.cell(int(date_message.todayD) + 1, 3).value
+        today_dinner = sheet.cell(int(date_message.todayD) + 1, 4).value
 
-    if i == tomorrowD:
-        tomorrow_breakfast = sheet.cell(tomorrowD + 1,2).value
-        tomorrow_lunch = sheet.cell(tomorrowD + 1, 3).value
-        tomorrow_dinner = sheet.cell(tomorrowD + 1, 4).value
+    if i == int(date_message.tomorrowD):
+        tomorrow_breakfast = sheet.cell(int(date_message.tomorrowD) + 1,2).value
+        tomorrow_lunch = sheet.cell(int(date_message.tomorrowD) + 1, 3).value
+        tomorrow_dinner = sheet.cell(int(date_message.tomorrowD) + 1, 4).value
 
-    if i == yesterdayD:
-        yesterday_breakfast = sheet.cell(yesterdayD + 1,2).value
-        yesterday_lunch = sheet.cell(yesterdayD + 1, 3).value
-        yesterday_dinner = sheet.cell(yesterdayD + 1, 4).value
+    if i == int(date_message.yesterdayD):
+        yesterday_breakfast = sheet.cell(int(date_message.yesterdayD) + 1,2).value
+        yesterday_lunch = sheet.cell(int(date_message.yesterdayD) + 1, 3).value
+        yesterday_dinner = sheet.cell(int(date_message.yesterdayD) + 1, 4).value
         
 def reload_jjambab():
-    date_message.reload_today()
     
-    todayD = int(date_message.todayD)
-    todayM = int(date_message.todayM)
-    tomorrowD = int(date_message.tomorrowD)
-    yesterdayD= int(date_message.yesterdayD)
+    date_message.reload_today()
     
     for i in range(0, 32):
 
-        if i == todayD:
-            today_breakfast = sheet.cell(todayD + 1,2).value
-            today_lunch = sheet.cell(todayD + 1, 3).value
-            today_dinner = sheet.cell(todayD + 1, 4).value
+        if i == int(date_message.todayD):
+            today_breakfast = sheet.cell(int(date_message.todayD) + 1,2).value
+            today_lunch = sheet.cell(int(date_message.todayD) + 1, 3).value
+            today_dinner = sheet.cell(int(date_message.todayD) + 1, 4).value
 
-        if i == tomorrowD:
-            tomorrow_breakfast = sheet.cell(tomorrowD + 1,2).value
-            tomorrow_lunch = sheet.cell(tomorrowD + 1, 3).value
-            tomorrow_dinner = sheet.cell(tomorrowD + 1, 4).value
+        if i == int(date_message.tomorrowD):
+            tomorrow_breakfast = sheet.cell(int(date_message.tomorrowD) + 1,2).value
+            tomorrow_lunch = sheet.cell(int(date_message.tomorrowD) + 1, 3).value
+            tomorrow_dinner = sheet.cell(int(date_message.tomorrowD) + 1, 4).value
 
-        if i == yesterdayD:
-            yesterday_breakfast = sheet.cell(yesterdayD + 1,2).value
-            yesterday_lunch = sheet.cell(yesterdayD + 1, 3).value
-            yesterday_dinner = sheet.cell(yesterdayD + 1, 4).value
+        if i == int(date_message.yesterdayD):
+            yesterday_breakfast = sheet.cell(int(date_message.yesterdayD) + 1,2).value
+            yesterday_lunch = sheet.cell(int(date_message.yesterdayD) + 1, 3).value
+            yesterday_dinner = sheet.cell(int(date_message.yesterdayD) + 1, 4).value
                 
 def search_jjambab(result):
     date_message.reload_today()
